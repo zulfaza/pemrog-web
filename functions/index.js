a62dc85ad4cf74a5c17e4af4774bfb92da7143ce
zulfaza <32305083+zulfaza@users.noreply.github.com>
2020-11-30 20:59:15 +0700

    terapin desain

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,36 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-const admin = require('firebase-admin')
-admin.initializeApp()
+const admin = require("firebase-admin");
+admin.initializeApp();
 const db = admin.firestore();
 
-app.post('/regis',async (req,res)=>{
-    
-    const {uid} = req.body
+app.post("/regis", async (req, res) => {
+  const { uid, displayName } = req.body;
 
-    return await db.collection('Users').doc(uid).set({
-        ListResep : [],
-        uid:uid
-    }).then(()=>{
-        return res.json({pesan:'berhasil membuat data'})
-    }).catch(err=>{
-        console.log(err);
-        return res.status(500).json({pesan:'gagal'})
+  return await db
+    .collection("Users")
+    .doc(uid)
+    .set({
+      displayName: displayName,
+      ListResep: [],
+      uid: uid,
     })
-})
+    .then(() => {
+      return res.json({ pesan: "berhasil membuat data" });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ pesan: "gagal" });
+    });
+});
 
-app.get('/coba',(req,res)=>{
-    return res.json({pesan:'ini get'})
-})
-app.post('/coba',(req,res)=>{
-    return res.json({pesan:'ini post'})
-})
+app.get("/coba", (req, res) => {
+  return res.json({ pesan: "ini get" });
+});
+app.post("/coba", (req, res) => {
+  return res.json({ pesan: "ini post" });
+});
 exports.api = functions.region("asia-southeast2").https.onRequest(app);
